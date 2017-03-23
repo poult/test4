581c70a2330604171dd6896219df21aeccf4c21f
Pavel <poult@mail.ru>
2017-03-23 08:26:07 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 23.03.2017
+ * Time: 7:58
+ */
+$companies = [
+    "Microsoft" => ["Programmer", "PR", "Office Manager"],
+    "Google" => ["IT", "Web-design"],
+    "Mozilla" => ["PR", "C++ Programmer"]
+];
+
+// можете используйте тег pre для форматирования вывода
+echo "<pre>";
+print_r($companies);
+echo "</pre>";
