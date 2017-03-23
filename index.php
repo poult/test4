01e7083968acb6b528895166203aed21b41544ae
Pavel <poult@mail.ru>
2017-03-23 09:35:25 +0300

    commit 10

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: admin
+ * Created by Pavel Tsikalovsky.
+ * User: poult
  * Date: 23.03.2017
  * Time: 7:58
  */
-$companies = [
-    "Microsoft" => ["Programmer", "PR", "Office Manager"],
-    "Google" => ["IT", "Web-design"],
-    "Mozilla" => ["PR", "C++ Programmer"]
-];
-//***************
-//***************
-//***************
-
-// можете используйте тег pre для форматирования вывода
-echo "<pre>";
-print_r($companies);
-echo "</pre>";
